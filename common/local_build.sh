1d2eddc53201c67f7b8cb1e4a56b9624534f8641
Mauro Riva <info@lemariva.com>
2025-11-01 02:59:08 +0100

    increase version to clean cache

--- common/local_build.sh
+++ common/local_build.sh
@@ -19,7 +19,7 @@
 
 docker buildx build --platform linux/aarch64 \
     -t lemariva/hassio-haproxy-certbot-addon:1.1.0 \
-    --build-arg SEMVER_REVISION=1.1.0 \
+    --build-arg SEMVER_REVISION=1.1.1 \
     --build-arg ARCHITECTURE=aarch64 \
     --build-arg BUILD_DATE=0 \
     --build-arg VCS_REF=0 \
